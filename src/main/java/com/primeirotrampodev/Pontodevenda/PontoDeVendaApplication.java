f4b441c1a2e6c3cd34d0abd019b14632735a5957
LucasMiranda <lucaswespras@gmail.com>
2023-04-18 20:58:40 -0300

    inicio

---
+++ src/main/java/com/primeirotrampodev/Pontodevenda/PontoDeVendaApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.Pontodevenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoDeVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoDeVendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/Pontodevenda/PontoDeVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.Pontodevenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoDeVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
